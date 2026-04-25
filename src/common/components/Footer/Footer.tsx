a6b3e3e002b6274b34e3d5cea5d3492249c73cb1
mxtrd <maxtrod27@gmail.com>
2026-04-25 15:25:03 +0500

    feat: implement footer layout and styles

--- src/common/components/Footer/Footer.tsx
+++ src/common/components/Footer/Footer.tsx
@@ -1,10 +1,14 @@
 import s from './Footer.module.scss'
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
     <footer className={s.footer}>
       <div className="container">
-        <div>footer</div>
+        <div className={s.inner}>
+          <div>© {currentYear} Kinopoisk Demo · Data courtesy of TMDB.</div>
+        </div>
       </div>
     </footer>
   )
